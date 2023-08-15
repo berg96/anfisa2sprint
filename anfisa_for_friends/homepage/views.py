6569c585516fc556a789a3c977439275318141ed
Artem Kulikov <chigar2010@yandex.ru>
2023-08-15 14:14:32 +0200

    rework homepage.index()

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -7,11 +7,12 @@ from ice_cream.models import IceCream
 def index(request):
     template = 'homepage/index.html'
     ice_cream_list = IceCream.objects.values(
-        'id', 'title', 'description', 'category__title'
-        ).filter(
-        Q(is_published=True) & 
-        (Q(title__contains='пломбир') | Q(is_on_main=True))
-        )[1:4]
+        'id', 'title', 'price', 'description'
+    ).filter(
+        is_published=True,
+        is_on_main=True,
+        category__is_published=True
+    )
     context = {
         'ice_cream_list': ice_cream_list,
     }
